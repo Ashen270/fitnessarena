6bf594188c2c3437e91b124891b348ed16c76d8f
Ashen Shamilka Wickramasinghe <ashenshamilka270@gmail.com>
2024-02-03 00:10:46 +0530

    updateApp.js

--- fitnessarena/src/App.js
+++ fitnessarena/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Hero from './componets/Hero/Hero';
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
-    </div>
+          <Hero />
+
+          
+     </div>   
   );
 }
 
---
+++ fitnessarena/src/componets/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className="Hero">
+        <div className="left-h">Left</div>
+        <div className="right-h">Right</div>
+
+
+    </div>
+
+  )
+}
+
+export default Hero
