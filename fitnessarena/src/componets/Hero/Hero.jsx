3650946ee53b35824096e1699d88d16a138cf08a
Ashen Shamilka Wickramasinghe <ashenshamilka270@gmail.com>
2024-02-14 04:11:33 +0530

    hero updated

--- fitnessarena/src/componets/Hero/Hero.jsx
+++ fitnessarena/src/componets/Hero/Hero.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Hero.css'
+import Header from '../Header/Header'
 
 const Hero = () => {
   return (
     <div className="Hero">
-        <div className="left-h">Left</div>
+        <div className="left-h">
+          <Header/>
+        </div>
         <div className="right-h">Right</div>
 
 
