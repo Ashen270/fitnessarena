547926d7905e8a363a819e43bdafaf219ce96f43
Ashen Shamilka Wickramasinghe <ashenshamilka270@gmail.com>
2024-02-16 23:45:03 +0530

    update-programs

--- fitnessarena/src/componets/programs/Programs.jsx
+++ fitnessarena/src/componets/programs/Programs.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Programs.css'
+import {programsData} from '../../data/programsData'
+import RightArrow from '../../assets/rightArrow.png'
 const Programs = () => {
   return (
     <div className="Programs" id = "programs">
@@ -9,6 +11,31 @@ const Programs = () => {
             <span>Programs</span>
             <span className='stroke-text'>to shape yoy</span> 
         </div>
+
+        <div className="program-categories">
+         {programsData.map((program)=>(
+          
+          <div className="category">
+            {program.image}
+            <span>{program.heading}</span>
+            <span>{program.details}</span>
+
+           <div className="join-now">
+            <span>Join Now</span>
+            <img src={RightArrow}/>
+           </div>
+
+          </div>
+         
+         ))}
+
+
+        </div>
+
+
+
+
+
     </div>
   )
 }
