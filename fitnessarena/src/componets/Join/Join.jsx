072a2d99c06a982661c424e6d55949f9d7e49258
Ashen Shamilka Wickramasinghe <ashenshamilka270@gmail.com>
2024-02-20 23:04:07 +0530

    jin updated

--- fitnessarena/src/componets/Join/Join.jsx
+++ fitnessarena/src/componets/Join/Join.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import './Join.css'
 
 const Join = () => {
   return (
-    <div>Join</div>
+   
+    <div className='Join' id='Join-us'>
+         
+         <div className="left-j">
+         <hr />
+           <div>
+            <span className='stroke-text'>Ready to</span>
+            <span> Level up</span>
+          </div>
+          <div>
+            <span>Your body</span>
+            <span className='stroke-text'>With us ?</span>
+          </div>
+          
+          
+         </div>
+         <div className="right-j">
+
+              <form action='' className='email-conatiner'>
+                <input type="email" name="user_email" placeholder='Enter Your Email Address'/>
+                <button className='btn btn-j'>Join us</button>
+              </form>
+
+
+         </div>
+
+
+    </div>
+
+
   )
 }
 
